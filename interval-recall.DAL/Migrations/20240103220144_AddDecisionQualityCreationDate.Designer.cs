4018b68dd495afe9c845627af2cd57d1d2ee314d
Kamil <karatov7@gmail.com>
2024-01-04 01:07:53 +0300

    feat(DecisionQuality): add the field CreationDate

--- interval-recall.DAL/Migrations/20240103220144_AddDecisionQualityCreationDate.Designer.cs
+++ interval-recall.DAL/Migrations/20240103220144_AddDecisionQualityCreationDate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using interval_recall.DAL.EF;
 
@@ -11,9 +12,11 @@ using interval_recall.DAL.EF;
 namespace interval_recall.DAL.Migrations
 {
     [DbContext(typeof(IntervalRecallContext))]
-    partial class IntervalRecallContextModelSnapshot : ModelSnapshot
+    [Migration("20240103220144_AddDecisionQualityCreationDate")]
+    partial class AddDecisionQualityCreationDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
